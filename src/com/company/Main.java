f99629ecd3e1dc9c8ab7bc731e6c8cef8d7422a9
tw073 <tw073@pvsd.org>
2019-03-19 10:59:12 -0400

    Finished Activity 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
-public class Main {
+public class Main { //This is the card tester class
 
     public static void main(String[] args) {
-	// write your code here
+
+        Card c1 = new Card("Ace", "Spades", 1);
+        Card c2 = new Card("3", "Clubs", 3);
+        Card c3 = new Card("Ace", "Spades", 1);
+
+        System.out.println(c1.matches(c2));
+        System.out.println(c1.matches(c3));
+
     }
 }
